b0c23d56612dc1193874973f05d82c5a763593ce
Seongmin <sjeon466@gmail.com>
2018-11-17 00:12:00 -0800

    add reverse sort logic

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -47,9 +47,14 @@ class Movies extends Component {
   };
 
   handleSort = path => {
-    this.setState({
-      sortColumn: { path, order: "asc" }
-    });
+    const sortColumn = { ...this.state.sortColumn };
+    if (sortColumn.path === path)
+      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
+    else {
+      sortColumn.path = path;
+      sortColumn.order = "asc";
+    }
+    this.setState({ sortColumn });
   };
 
   /*   handleFiler = selectedGenre => {
