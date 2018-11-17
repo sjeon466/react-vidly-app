e9ea5c6cd3ed7037d96785145e0728bc2a9a245c
Seongmin <sjeon466@gmail.com>
2018-11-16 22:43:59 -0800

    implement logic for pagination and listgroup

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -44,13 +44,29 @@ class Movies extends Component {
     this.setState({ selectedGenre: genre });
   };
 
+  /*   handleFiler = selectedGenre => {
+    const movies = this.state.movies.map(
+      movie => this.state.movies.genre === movie.genre
+    );
+    this.setState(movies);
+  };
+ */
   render() {
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage, movies: allMovies } = this.state;
+    const {
+      pageSize,
+      currentPage,
+      movies: allMovies,
+      selectedGenre
+    } = this.state;
     if (count === 0) return <p>There are no movies in the database.</p>;
 
+    const filtered = selectedGenre
+      ? allMovies.filter(m => selectedGenre._id === m.genre._id)
+      : allMovies;
+
     //const movies = ...
-    const movies = paginate(allMovies, currentPage, pageSize);
+    const movies = paginate(filtered, currentPage, pageSize);
 
     return (
       <div className="row">
@@ -62,7 +78,7 @@ class Movies extends Component {
           />
         </div>
         <div className="col">
-          <p>Showing {count} movies in the database.</p>
+          <p>Showing {filtered.length} movies in the database.</p>
           <table className="table">
             <thead>
               <tr>
@@ -100,7 +116,7 @@ class Movies extends Component {
             </tbody>
           </table>
           <Pagination
-            itemsCount={count}
+            itemsCount={filtered.length}
             pageSize={pageSize}
             currentPage={currentPage}
             onPageChange={this.handlePageChange}
