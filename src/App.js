b4ade6c98ac80ffc76ac44b4fb85a53f58d9bcef
Seongmin <sjeon466@gmail.com>
2018-11-18 23:30:45 -0800

    link to movieform

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import NavBar from "./components/navbar";
+import MovieForm from "./components/movieForm";
 import "./App.css";
 
 class App extends Component {
@@ -14,6 +15,8 @@ class App extends Component {
         <NavBar />
         <main className="container">
           <Switch>
+            <Route path="/movies/:id" component={MovieForm} />
+
             <Route path="/movies" component={Movies} />
             <Route path="/customers" component={Customers} />
             <Route path="/rentals" component={Rentals} />
