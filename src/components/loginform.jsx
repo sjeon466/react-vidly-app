f92d0e288cdfb808e36d7887a6667ab64ea931e9
Seongmin <sjeon466@gmail.com>
2018-11-19 04:04:45 -0800

    make multiple controlled input

--- src/components/loginform.jsx
+++ src/components/loginform.jsx
@@ -1,18 +1,52 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
+  state = {
+    account: { username: "", password: "" }
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log("Submitted");
+  };
+
+  handleChange = ({ currentTaget: input }) => {
+    const account = {
+      ...this.state.account
+    };
+
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>login</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control" />
+            <input
+              value={account.username}
+              onChange={this.handleChange}
+              autoFocus
+              ref={this.username}
+              name="username"
+              id="username"
+              type="text"
+              className="form-control"
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              value={account.password}
+              onChange={this.handleChange}
+              name="password"
+              id="password"
+              type="text"
+              className="form-control"
+            />
           </div>
         </form>
         <button className="btn btn-primary">Login</button>
