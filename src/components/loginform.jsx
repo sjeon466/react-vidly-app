6f8f3add1d7994101b6f0c30a19ae6477066ea32
Seongmin <sjeon466@gmail.com>
2018-11-19 22:03:07 -0800

    refactor joi validation

--- src/components/loginform.jsx
+++ src/components/loginform.jsx
@@ -9,25 +9,22 @@ class LoginForm extends Component {
   };
 
   schema = {
-    username : Joi.string().required(),
-    password : Joi.string().required()
+    username : Joi.string().required().label("Username"),
+    password : Joi.string().required().label("Password")
   }
 
-  validate = e => {
-    const result = Joi.validete(this.state.account, this.schema);
-    console.log(result);
+  validate = () => {
+    const options = {abortEarly : false};
+    const {error} = Joi.validete(this.state.account, this.schema, options);
+    if(!error) return null;
 
-    const errors = {};
-    const {account } = this.state;
+    const errors ={};
 
-    if(account.username.trim() === '')
-      errors.username = "Username is required.";
-
-    if(account.password.trim() === '')
-      errors.password = "Password is required.";
-
-    return Object.key(errors).length === 0 ? null : errors;
-  }
+    for(let item of result.error.details){
+      errors[item.path[0]] = item.message;
+    }
+    return errors;
+  };
 
   handleSubmit = e => {
     e.preventDefault();
@@ -39,7 +36,14 @@ class LoginForm extends Component {
   };
 
   validateProperty = ({name, value}) => {
-    if(name === "username"){
+    const obj = { [name] : value};
+    const schema = { [name] : this.schema[name]};
+    const {error} = Joi.validate(obj,schema);
+    if(error) return null;
+    return error.details[0].message;
+    
+
+/*     if(name === "username"){
       if(value.trim()=== "") return "Username is required";
       // other rules
     }
@@ -47,7 +51,7 @@ class LoginForm extends Component {
     if(name === "password"){
       if(value.trim()=== "") return "password is required";
       // other rules
-    }
+    } */
   };
 
   handleChange = ({ currentTaget: input }) => {
