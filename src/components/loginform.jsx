90ae01c8af1ac66cf3a42b0176641c6259f89a31
Seongmin <sjeon466@gmail.com>
2018-11-24 01:23:54 -0800

    add redirecting after login logic

--- src/components/loginform.jsx
+++ src/components/loginform.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Joi from "joi-browser";
 import Form from "./common/form";
 import auth from "../services/authService";
+import { Redirect } from "react-router-dom";
 
 class LoginForm extends Form {
   state = {
@@ -26,6 +27,8 @@ class LoginForm extends Form {
       await auth.login(data.username, data.password);
       //console.log(jwt);
       //this.props.history.push("/");
+      const { state } = this.props.location;
+      window.location = state ? state.from.pathname : "/";
       window.location = "/";
     } catch (ex) {
       if (ex.response && ex.resopnse.status === 400) {
@@ -37,6 +40,8 @@ class LoginForm extends Form {
   };
 
   render() {
+    if (auth.getCurrentUser()) return <Redirect to="/" />;
+
     return (
       <div>
         <h1>Login</h1>
