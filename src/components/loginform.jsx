6a23e05b2d2088c956af44bf490539f7dce42f67
Seongmin <sjeon466@gmail.com>
2018-11-23 20:17:30 -0800

    add login logic

--- src/components/loginform.jsx
+++ src/components/loginform.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Joi from "joi-browser";
 import Form from "./common/form";
+import { login } from "../services/authService";
 
 class LoginForm extends Form {
   state = {
@@ -17,9 +18,11 @@ class LoginForm extends Form {
       .label("Password")
   };
 
-  doSubmit = () => {
+  doSubmit = async () => {
     // Call the server
-    console.log("Submitted");
+    //console.log("Submitted");
+    const { data } = this.state;
+    await login(data.username, data.password);
   };
 
   render() {
