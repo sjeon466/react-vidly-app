2fd7fb4bf09052907dc7273c7d9110bb2773ec65
Seongmin <sjeon466@gmail.com>
2018-11-23 20:10:21 -0800

    add register form logic

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Joi from "joi-browser";
 import Form from "./common/form";
+import * as userService from "../services/userService";
 
 class RegisterForm extends Form {
   state = {
@@ -22,9 +23,18 @@ class RegisterForm extends Form {
       .label("Name")
   };
 
-  doSubmit = () => {
+  doSubmit = async () => {
     // Call the server
-    console.log("Submitted");
+    //console.log("Submitted");
+    try {
+      await userService.register(this.state.data);
+    } catch (ex) {
+      if (ex.response && ex.resopnse.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = ex.response.data;
+        this.setState({ errors });
+      }
+    }
   };
 
   render() {
